e3da5fc1770fde393a743268400d68a112eb5352
UTKARSH SAINI <utkarshsaini19@gmail.com>
2023-05-19 01:35:13 +0530

    Again modified
    Again modified

--- server.ts
+++ server.ts
@@ -6,6 +6,10 @@ import cors from 'cors';
 import path from 'path';
 
 
+console.log(__dirname);
+
+
+
 
 
 const app:express.Application = express()
@@ -20,10 +24,10 @@ app.use(express.urlencoded({extended:false}))
 app.use('/api',apiRouter)
 app.use('/user',userRouter)
 
-app.use(express.static(path.join(__dirname, "./demoapp/build")));
+app.use(express.static(path.join(__dirname, "demoapp","build")));
     app.get("*", function (_, res) {
       res.sendFile(
-        path.join(__dirname, "./demoapp/build/index.html"),
+        path.join(__dirname, "demoapp","build","index.html"),
         function (err) {
           res.status(500).send(err);
         }
